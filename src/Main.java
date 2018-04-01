6b26d16c475f564919f5aacbea203d89b8813e72
mygetzu <mbenypangestu@it.student.pens.ac.id>
2018-04-01 22:42:44 +0700

    Update - game starting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-
     public static void main(String args[]) throws Exception {
         int stick, choose_move;
 
